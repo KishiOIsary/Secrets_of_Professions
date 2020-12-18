342a99ed1e11baff8741085d2357d60c226a880f
Dmitriy <mrmda28@gmail.com>
2020-12-18 12:35:15 +0300

    Added transfer from scenes

--- Assets/Resources/Scripts/Scene_1.cs
+++ Assets/Resources/Scripts/Scene_1.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
-public class Scene_1 : MonoBehaviour
+public class Scene_1 : MonoBehaviour,IPointerClickHandler
 {
     // Start is called before the first frame update
     void Start()
@@ -10,9 +13,8 @@ public class Scene_1 : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public void OnPointerClick(PointerEventData eventData){
+    	GetComponent<Image>().color= new Color(1f,0.4f,0.4f,0.5f);
+    	SceneManager.LoadScene("Scene_1");
+    } 
 }
