685f21ec667916c57328b781f131fbdae827c28a
Dmitriy <mrmda28@gmail.com>
2020-12-18 11:53:38 +0300

    Added hover to menu

---
+++ Assets/Resources/Scripts/MouseHover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MouseHover : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData){
+    	GetComponent<Image>().color= new Color(0.4f,0.4f,0.4f,0.5f);
+    } 
+
+    public void OnPointerExit(PointerEventData eventData){
+    	GetComponent<Image>().color= Color.clear;
+    } 
+}
